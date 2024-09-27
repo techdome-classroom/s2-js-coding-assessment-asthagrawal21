5bf56a6eefa23d0a2f2f3e7775b9c69528f5a272
asthagrawal21 <officialasthagrawal@gmail.com>
2024-09-27 14:05:15 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -8,4 +8,22 @@ var isValid = function(s) {
 
 module.exports = { isValid };
 
-
+function isValid(s) {
+    let stack = [];
+    let map = {
+        '(' : ')',
+        '[' : ']', 
+        '{' : '}'
+    } ;
+    for(let i = 0 ; i < s.length; i++ ) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[' ) {
+            stack.push(s[i]);
+        }
+        else {
+            if (stack.length === 0 || map[stack.pop()] !== s[i]){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
--- program1.md
+++ program1.md
@@ -10,12 +10,18 @@ Open brackets must be closed by the same type of brackets. Open brackets must be
 
 Example 1:
 
-Input: s = "()" Output: true Example 2:
+Input: s = "()" Output: true 
 
-Input: s = "()[]{}" Output: true Example 3:
+Example 2:
+
+Input: s = "()[]{}" Output: true 
+
+Example 3:
 
 Input: s = "(]" Output: false
 
 Constraints:
 
 1 <= s.length <= 104 s consists of parentheses only '()[]{}'.
+
+
